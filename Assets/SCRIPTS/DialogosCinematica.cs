b0ac713ef07ff13ec4c545bee405ea212d93e348
Slava <superm30@hotmail.com>
2017-11-03 13:06:02 +0100

    CINEMATICAS

---
+++ Assets/SCRIPTS/DialogosCinematica.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogosCinematica : MonoBehaviour {
+
+
+	public Text texto;
+	public string[] frases;
+	private int contador = 0;
+
+	// Use this for initialization
+	void Start () {
+		texto.text = frases [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contador < frases.Length) {
+			if (Input.GetKeyDown ("space")) {
+				contador++;
+				texto.text = frases [contador];
+			}
+		} 
+
+		else{
+			//carga siguiente nivel
+		}
+	}
+}
