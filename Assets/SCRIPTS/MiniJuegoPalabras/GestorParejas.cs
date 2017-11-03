67ee2f036feea14db4608df8f690dd98f31f63ce
Japend <asdfasdf@gmail.com>
2017-11-03 13:16:11 +0100

    Palabras

---
+++ Assets/SCRIPTS/MiniJuegoPalabras/GestorParejas.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestorParejas : MonoBehaviour {
+
+    public float xInicial;
+    public float yInicialVerbos, yInicialNombres;
+    public float incrementoX, incrementoY;
+    public int numeroPanelesPorFila;
+
+
+    SecuenciasAcciones secuencias;
+    Secuencia secuenciaActiva;
+    int indiceSecuenciaActiva;
+
+    //paneles base
+    GameObject soporteNombre;
+    GameObject soporteVerbo;
+
+    GameObject canvas;
+
+    //paneles activos
+    List<GameObject> panelesNombresActivos;
+    List<GameObject> panelesVerbosActivos;
+
+	// Use this for initialization
+	void Awake () {
+        secuencias = SecuenciasAcciones.Getinstance();
+
+        //soportes palabras
+        soporteNombre = GameObject.Find("SoporteNombre");
+        soporteNombre.SetActive(false);
+        soporteVerbo = GameObject.Find("SoporteVerbo");
+        soporteVerbo.SetActive(false);
+
+        //canvas, para que sea el padre de los soportes de palbras
+        canvas = GameObject.Find("Canvas");
+
+        indiceSecuenciaActiva = 0;
+        secuenciaActiva = secuencias.secuencias[indiceSecuenciaActiva];
+
+        panelesNombresActivos = new List<GameObject>();
+        panelesVerbosActivos = new List<GameObject>();
+
+        cargarSecuencia();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void cargarSecuencia()
+    {
+        GameObject aux;
+
+        //generar paneles para los nombres
+        for(int i = 0; i < secuenciaActiva.nombres.Length; i++)
+        {
+            //si el soporte ya existe, se actualiza el texto
+            if (i < panelesNombresActivos.Count)
+            {
+                panelesNombresActivos[i].GetComponentInChildren<Text>().text = secuenciaActiva.nombres[i];
+                panelesNombresActivos[i].SetActive(true);
+            }
+
+            //si no existe, se crea uno nuevo 
+            else
+            {
+                aux = GameObject.Instantiate(soporteNombre, canvas.transform);
+                aux.GetComponentInChildren<Text>().text = secuenciaActiva.nombres[i];
+                aux.SetActive(true);
+                panelesNombresActivos.Add(aux);
+            }
+
+            panelesNombresActivos[i].transform.localPosition = new Vector3(xInicial + (i % numeroPanelesPorFila * incrementoX),
+                                                       yInicialNombres - (incrementoY * (i / numeroPanelesPorFila)), -2f);
+        }
+
+        //si han sobrado paneles, se desactivan
+        for (int i = panelesNombresActivos.Count - 1; i >= secuenciaActiva.nombres.Length; i--)
+        {
+            panelesNombresActivos[i].SetActive(false);
+        }
+
+
+        //hacemos lo mismo con los verbos
+        for (int i = 0; i < secuenciaActiva.verbos.Length; i++)
+        {
+            //si el soporte ya existe, se actualiza el texto
+            if (i < panelesVerbosActivos.Count)
+            {
+                panelesVerbosActivos[i].GetComponentInChildren<Text>().text = secuenciaActiva.verbos[i];
+                panelesVerbosActivos[i].SetActive(true);
+            }
+
+            //si no existe, se crea uno nuevo 
+            else
+            {
+                aux = GameObject.Instantiate(soporteVerbo, canvas.transform);
+                aux.GetComponentInChildren<Text>().text = secuenciaActiva.verbos[i];
+                aux.SetActive(true);
+                panelesVerbosActivos.Add(aux);
+            }
+
+            panelesVerbosActivos[i].transform.localPosition = new Vector3(xInicial + (i % numeroPanelesPorFila * incrementoX),
+                                                        yInicialVerbos - (incrementoY * (i / numeroPanelesPorFila)), -2f);
+                                                                   
+        }
+
+        //si han sobrado paneles, se desactivan
+        for (int i = panelesVerbosActivos.Count - 1; i >= secuenciaActiva.verbos.Length; i--)
+        {
+            panelesVerbosActivos[i].SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/SCRIPTS/Recursos/SecuenciasAcciones.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Pair<T>
+{
+    private T first;
+    private T second;
+
+    Pair(T f, T s)
+    {
+        first = f;
+        second = s;
+    }
+
+    public T First
+    {
+      get { return first; }
+    }
+    public T Second
+    {
+      get { return second; }
+    }
+}
+
+public struct Secuencia
+{
+    /*
+     * Almacena todas las palabras de cada secuencia. Las parejas validas sona las correspondientes a los indices entre 0 y'parejas' (sin incluirlo)
+     * es decir, si nombres y verbos tienen una longitud de 5 y parejas = 3, las parejas validas seran (0,0), (1,1) y (2,2). el resto de combinaciones no seran validas 
+     */
+
+    public string[] nombres;
+    public string[] verbos;
+
+    public int parejas;
+
+    public Secuencia(string[] palsA, string[] palsB, int parejas)
+    {
+        nombres = palsA;
+        verbos = palsB;
+        this.parejas = parejas;
+    }
+}
+
+public class SecuenciasAcciones{
+
+    static SecuenciasAcciones instanciaActiva;
+
+    public Secuencia[] secuencias = new Secuencia[]
+        {
+            //abrir puerta
+            new Secuencia(
+                new string[]{"portal", "pomo", "pomo", "puerta", "escalera", "visillo"},
+                new string[]{"acercarse", "coger", "girar", "empujar", "golpear", "sacar"},
+                3),
+            //sacar DNI
+             new Secuencia(
+                new string[]{"cartera", "cartera", "DNI", "DNI", "DNI", "tarjeta", "carné", "monedas", "bolsillo"},
+                new string[]{"sacar", "abrir", "buscar", "coger", "sacar", "contar", "observar", "leer"},
+                5),
+
+        };
+
+
+
+    public static SecuenciasAcciones Getinstance()
+    {
+        if (instanciaActiva == null)
+            instanciaActiva = new SecuenciasAcciones();
+        return instanciaActiva;
+
+    }
+}
